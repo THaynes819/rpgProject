2d7697f26f3c42e9bc6931ecd37892f34a59163a
mogstream <tracyehaynes@gmail.com>
2019-11-11 14:34:43 -0500

    Initial Late Commit.

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+   [SerializeField] Transform target;
+
+    Camera playerCamera;
+    
+    void Update()
+    {
+        transform.position = target.position;
+    }
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+
+{
+    
+    Camera playerCamera;    
+    NavMeshAgent agent;
+   
+    void Update()
+    {
+        playerCamera = Camera.main;
+        if (Input.GetButton("Fire1"))
+        {                      
+            MoveToCursor();
+        }
+    }    
+
+    private void MoveToCursor()
+    {
+        Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        agent = GetComponent<NavMeshAgent>();
+
+        if(hasHit)
+        {
+            agent.destination = hit.point;
+        }
+    }    
+}
