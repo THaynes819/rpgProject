a87023412779e01eae1a6bff3eeae956b400b70a
mogstream <tracyehaynes@gmail.com>
2020-04-28 20:52:58 -0400

    First Cinematic

---
+++ Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace RPG.Cinematics
+{   
+    public class CinematicControlRemover : MonoBehaviour 
+    {
+
+        private void Start() 
+        {
+            
+        }
+
+        void DisableControl()
+        {
+            print("DisableControl")
+        }
+
+        void EnableControl()
+        {
+            print("EnableControl")
+        }
+    }
+}
---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+
+    bool alreadyTriggered = false;
+
+    GameObject player;
+
+        private void Start() 
+        {
+            player = GameObject.FindWithTag("Player");
+        }
+
+        private void OnTriggerEnter(Collider other) 
+        {            
+            if (!alreadyTriggered && other.gameObject == player)
+            {                
+                GetComponent<PlayableDirector>().Play();
+                alreadyTriggered = true;               
+            }
+            else
+            {
+                return;
+            }    
+        }       
+    }
+
+}
