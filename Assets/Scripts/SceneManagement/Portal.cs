ae5a41865e5f8f060c7cf18b7db1aaf7495a123e
mogstream <tracyehaynes@gmail.com>
2020-05-01 20:37:21 -0400

    Setup destination ID's for portals. created fader for level loads.

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,37 +1,70 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using System;
+using UnityEngine.AI;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
 
-        [SerializeField] int sceneToLoad = 1;
-        
-        GameObject player;
-
-
-        private void Start()
+        enum DestinationIdentifier
         {
-            player = GameObject.FindWithTag("Player");
-            
+            A, B, C, D, E
         }
 
+        [SerializeField] int sceneToLoad = 1;
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destinationPortal;
+        [SerializeField] DestinationIdentifier currentPortal;
+
+        
         private void OnTriggerEnter(Collider other) 
         {
-            if (other.gameObject == player)
+            if (other.gameObject == GameObject.FindWithTag("Player"))
             {            
                 StartCoroutine(Transition());
             }
         }
 
         private IEnumerator Transition()
-        {            
+        {           
+            if (sceneToLoad < 0)
+            {
+                Debug.LogError("Scene to load not set.");
+                yield break;
+            }
             DontDestroyOnLoad(gameObject);           
-            yield return SceneManager.LoadSceneAsync(sceneToLoad);
-            print("Scene Loaded");
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);   
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
             Destroy(gameObject);            
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");  
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);            
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                if (portal.currentPortal == destinationPortal )
+                {
+                    print("Leaving Portal " + portal.currentPortal + " and entering Portal " + portal.destinationPortal);
+                    return portal;
+                }
+            }
+            
+            return null;            
         }
     }
 }
