45d512c4d0184aee1e26ddd9b3f7bc91baf7e1eb
mogstream <tracyehaynes@gmail.com>
2020-05-03 19:16:58 -0400

    working on my own save system

--- Assets/Scripts/Saving/MadeByMe/SavingSystemMe.cs
+++ Assets/Scripts/Saving/MadeByMe/SavingSystemMe.cs
@@ -6,11 +6,12 @@ namespace RPG.Saving
     {
         public void Save(string saveFile)
         {
-            print("Saving to " + saveFile)
+            print("Saving to " + saveFile);
         }
 
         public void Load(string saveFile)
         {
-            print("Loading from " + saveFile)
+            print("Loading from " + saveFile);
         }
+    }    
 }
