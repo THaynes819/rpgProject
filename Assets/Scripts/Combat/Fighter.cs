c94f71a9e8476b3167d890cad4c15180c89cf9e8
mogstream <tracyehaynes@gmail.com>
2020-05-10 12:13:44 -0400

    Added weapons and pickups

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -6,13 +6,21 @@ namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour, IAction
     {
-        [SerializeField] float weaponRange = 2f;
-        [SerializeField] float timeBetweenAttacks = 1f;
-        [SerializeField] float weaponDamage = 2f;
+        
+        [SerializeField] float timeBetweenAttacks = 1f;               
+        [SerializeField] Transform handTransform = null;
+        [SerializeField] Weapon defaultweapon = null;
 
+        
         Health target;        
         float timeSinceLastAttack = Mathf.Infinity;
+        Weapon currentWeapon = null;
         
+       private void Start()
+       {
+           EquipWeapon(defaultweapon); 
+       }
+
         private void Update()
         {
             timeSinceLastAttack += Time.deltaTime;
@@ -30,6 +38,13 @@ namespace RPG.Combat
                 GetComponent<Mover>().Cancel();                
                 AttackBehaviour();
             }
+        }
+
+        public void EquipWeapon(Weapon weapon)
+        {       
+            currentWeapon = weapon;
+            Animator animator = GetComponent<Animator>();
+            weapon.Spawn(handTransform, animator); 
         }
 
         private void AttackBehaviour()
@@ -52,12 +67,12 @@ namespace RPG.Combat
         void Hit()
         {
             if (target == null) { return; }
-            target.TakeDamage(weaponDamage);
+            target.TakeDamage(currentWeapon.GetWeaponDamage());
         }
 
         private bool GetIsInRange()
         {
-            return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
+            return Vector3.Distance(transform.position, target.transform.position) < currentWeapon.GetWeaponRange();
         }
 
         public bool CanAttack(GameObject combatTarget)
@@ -86,5 +101,9 @@ namespace RPG.Combat
             GetComponent<Animator>().ResetTrigger("attack");
             GetComponent<Animator>().SetTrigger("stopAttack");
         }
+
+       
     }
+
+    
 }
---
+++ Assets/Scripts/Combat/Weapon.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace RPG.Combat
+{
+    [CreateAssetMenu(fileName = "Weapon", menuName = "Weapons/make New Weapon", order = 0)]
+    public class Weapon : ScriptableObject
+    {
+        [SerializeField] AnimatorOverrideController animatorOverride = null;
+        [SerializeField] GameObject eqqiuppedPrefab = null;
+        [SerializeField] float weaponRange = 2f;
+        [SerializeField] float weaponDamage = 10f;
+
+        public float GetWeaponRange()
+        {
+            return weaponRange;
+        }
+
+        public float GetWeaponDamage()
+        {
+            return weaponDamage;
+        }
+       
+        public void Spawn(Transform handtransform, Animator animator)
+        {
+            if(eqqiuppedPrefab != null)
+            {
+            Instantiate(eqqiuppedPrefab, handtransform);
+            }
+            
+            if(animatorOverride != null)
+            {
+            animator.runtimeAnimatorController = animatorOverride;
+            }
+        }
+    }
+}
