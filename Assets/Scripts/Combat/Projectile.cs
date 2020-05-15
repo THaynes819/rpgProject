ccba58e9245b5e026d249153873be5cda1da61e4
mogstream <tracyehaynes@gmail.com>
2020-05-14 20:35:01 -0400

    Added bow and arrow with improved functionality and Fireball.

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -9,19 +9,29 @@ namespace RPG.Combat
     public class Projectile : MonoBehaviour
     {
                 
-        [SerializeField] float projectileSpeed = 1f;
+        [SerializeField] float projectileSpeed = 10f;
+        [SerializeField] float projectileDistance = 40f;
+        [SerializeField] bool isHoming = false;
         Health target = null;
         float damage = 0f;
 
+        private void Start() 
+        {            
+            transform.LookAt(GetAimLoacation());
+        }
+        
         void Update()
         {
             if (target == null)
             {
                 return;
             }
-            transform.LookAt(GetAimLoacation());
+            if (isHoming && !target.IsDead())
+            {
+                transform.LookAt(GetAimLoacation());
+            }
             transform.Translate(Vector3.forward * projectileSpeed * Time.deltaTime);
-        }
+        }        
 
         public void SetTarget(Health target, float damage)
         {
@@ -41,8 +51,8 @@ namespace RPG.Combat
 
         private void OnTriggerEnter(Collider other)
         {   
-            if (other.GetComponent<Health>() != target)
-            {
+            if (other.GetComponent<Health>() != target || target.IsDead())
+            {                
                 return;
             }
             target.TakeDamage(damage); 
