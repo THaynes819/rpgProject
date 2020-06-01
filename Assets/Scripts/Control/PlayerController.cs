033be3407fe0931821f364cd510fffd6ae584f2d
mogstream <tracyehaynes@gmail.com>
2020-06-01 14:39:39 -0400

    Cursor now reacts appropriately to Navmesh Water effect created

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,8 +1,9 @@
-﻿using UnityEngine;
-using RPG.Movement;
+﻿using System;
 using RPG.Combat;
+using RPG.Movement;
 using RPG.Resources;
-using System;
+using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.EventSystems;
 
 namespace RPG.Control
@@ -22,49 +23,48 @@ namespace RPG.Control
         }
 
         [SerializeField] CursorMapping[] cursorMappings = null;
+        [SerializeField] float raycastMaxDistance = 1.0f;
 
-
-        private void Awake()
+        private void Awake ()
         {
-            health = GetComponent<Health>();
+            health = GetComponent<Health> ();
         }
 
-        private void Update()
+        private void Update ()
         {
-            if (InteractWithUI())
+            if (InteractWithUI ())
             {
-                SetGameCursor(CursorType.UI);
+                SetGameCursor (CursorType.UI);
                 return;
             }
-            if (health.IsDead())
+            if (health.IsDead ())
             {
-                SetGameCursor(CursorType.None);
+                SetGameCursor (CursorType.None);
                 return;
             }
 
-            if (InteractWithComponent()) { return; }
-            if (InteractWithMovement()) { return; }
+            if (InteractWithComponent ()) { return; }
+            if (InteractWithMovement ()) { return; }
 
-            SetGameCursor(CursorType.None);
+            SetGameCursor (CursorType.None);
         }
 
-
-        private bool InteractWithUI()
+        private bool InteractWithUI ()
         {
-            return EventSystem.current.IsPointerOverGameObject(); //gameobject means UI gameobject
+            return EventSystem.current.IsPointerOverGameObject (); //gameobject means UI gameobject
         }
 
-        private bool InteractWithComponent()
+        private bool InteractWithComponent ()
         {
-            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            RaycastHit[] hits = RaycastAllSorted ();
             foreach (RaycastHit hit in hits)
             {
-                IRaycastable[] raycastables = hit.transform.GetComponents<IRaycastable>();
+                IRaycastable[] raycastables = hit.transform.GetComponents<IRaycastable> ();
                 foreach (IRaycastable raycastable in raycastables)
                 {
-                    if (raycastable.HandleRaycast(this))
+                    if (raycastable.HandleRaycast (this))
                     {
-                        SetGameCursor(raycastable.GetCursorType());
+                        SetGameCursor (raycastable.GetCursorType ());
                         return true;
                     }
                 }
@@ -72,30 +72,59 @@ namespace RPG.Control
             return false;
         }
 
-
-        private bool InteractWithMovement()
+        RaycastHit[] RaycastAllSorted ()
         {
-            RaycastHit hit;
-            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+            RaycastHit[] hits = Physics.RaycastAll (GetMouseRay ());
+            float[] distances = new float[hits.Length];
+            for (int i = 0; i < hits.Length; i++)
+            {
+                distances[i] = hits[i].distance;
+            }
+            Array.Sort (distances, hits);
+            return hits;
+        }
+
+        private bool InteractWithMovement ()
+        {
+
+            Vector3 target;
+            bool hasHit = RaycsatNavmesh (out target);
             if (hasHit)
             {
-                if (Input.GetMouseButton(0))
+                if (Input.GetMouseButton (0))
                 {
-                    GetComponent<Mover>().StartMoveAction(hit.point, 1f);
+                    GetComponent<Mover> ().StartMoveAction (target, 1f);
                 }
-                SetGameCursor(CursorType.Movement);
+                SetGameCursor (CursorType.Movement);
                 return true;
             }
             return false;
         }
 
-        private void SetGameCursor(CursorType type)
+        private bool RaycsatNavmesh (out Vector3 target)
         {
-            CursorMapping mapping = GetCursorMapping(type);
-            Cursor.SetCursor(mapping.cursorTexture, mapping.hotSpot, CursorMode.Auto);
+            target = new Vector3 ();
+
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast (GetMouseRay (), out hit);
+            if (!hasHit) return false;
+
+            NavMeshHit navMeshHit;
+            bool hasCastToMesh = NavMesh.SamplePosition (hit.point, out navMeshHit, raycastMaxDistance, NavMesh.AllAreas);
+            if (!hasCastToMesh) return false;
+
+            target = navMeshHit.position;
+            return true;
+
         }
 
-        private CursorMapping GetCursorMapping(CursorType type)
+        private void SetGameCursor (CursorType type)
+        {
+            CursorMapping mapping = GetCursorMapping (type);
+            Cursor.SetCursor (mapping.cursorTexture, mapping.hotSpot, CursorMode.Auto);
+        }
+
+        private CursorMapping GetCursorMapping (CursorType type)
         {
             foreach (CursorMapping mapping in cursorMappings)
             {
@@ -107,12 +136,9 @@ namespace RPG.Control
             return cursorMappings[0];
         }
 
-        private static Ray GetMouseRay()
+        private static Ray GetMouseRay ()
         {
-            return Camera.main.ScreenPointToRay(Input.mousePosition);
+            return Camera.main.ScreenPointToRay (Input.mousePosition);
         }
-
-
     }
-
 }
