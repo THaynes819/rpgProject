8c439421dadfda7ff40b53dd5afff8e87ce6cba0
mogstream <tracyehaynes@gmail.com>
2020-06-01 19:49:05 -0400

    Added Raycasting to Navmesh to create more  flexabilty Set Max Distance to point and click movement Added cursor change to show when max movement distance is reached

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using RPG.Combat;
 using RPG.Movement;
 using RPG.Resources;
@@ -24,6 +25,7 @@ namespace RPG.Control
 
         [SerializeField] CursorMapping[] cursorMappings = null;
         [SerializeField] float raycastMaxDistance = 1.0f;
+        [SerializeField] float maxPathLength = 40f;
 
         private void Awake ()
         {
@@ -114,8 +116,27 @@ namespace RPG.Control
             if (!hasCastToMesh) return false;
 
             target = navMeshHit.position;
+
+            NavMeshPath path = new NavMeshPath ();
+            bool hasPath = NavMesh.CalculatePath (transform.position, target, NavMesh.AllAreas, path);
+            if (!hasPath) return false;
+            if (path.status != NavMeshPathStatus.PathComplete) return false;
+            if (GetPathLength (path) > maxPathLength) return false;
+
             return true;
 
+        }
+
+        private float GetPathLength (NavMeshPath path)
+        {
+            float total = 0;
+            if (path.corners.Length < 2f) return total;
+            for (int i = 0; i < path.corners.Length - 1; i++)
+            {
+                total += Vector3.Distance (path.corners[i], path.corners[i + 1]);
+            }
+
+            return total;
         }
 
         private void SetGameCursor (CursorType type)
