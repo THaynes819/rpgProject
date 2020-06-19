bb30b1f7cefed43c18c762ee97e7bce99c74b042
mogstream <tracyehaynes@gmail.com>
2020-06-18 20:53:21 -0400

    imported multiple asset packs implimented a working player health Globe with placeholder art

---
+++ Assets/Scripts/Attributes/HealthGlobe.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.Attributes
+{
+    public class HealthGlobe : MonoBehaviour
+    {
+
+        [SerializeField] Slider slider = null;
+        [SerializeField] Health player = null;
+
+        void Awake ()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update ()
+        {
+            slider.value = player.GetFraction();
+        }
+    }
+}
